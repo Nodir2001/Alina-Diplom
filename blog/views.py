a0a54b41e5e5429f2414c25f824339ba5993c500
Nodir2001 <rasulovnd@stud.kai.ru>
2021-05-03 13:22:31 +0300

    upgrading

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, 'blog/landing.html',)
+ 
