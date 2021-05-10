27bc2e42c4025709fdb871827b83a7b03fdae0bb
Nodir2001 <rasulovnd@stud.kai.ru>
2021-05-11 02:46:33 +0300

    upgredding

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mhee#$w8ifwjuqcbsh4i544(#ibnd^c34x3@1x)kee0p^ay40^'
-
+SECRET_KEY = 'mhee#$w8ifwjuqcbsh4i544(#ibnd^c34x3@1x)kee0p^ay40^' # str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -77,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':   'testdb',   # os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
